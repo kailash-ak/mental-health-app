17d4508e44f5f7631268bfdd2d00e4a686cba6a5
kailash-ak <137521066+kailash-ak@users.noreply.github.com>
2025-03-01 12:10:35 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import LandingPage from './pages/LandingPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import ForgotPassword from './pages/ForgotPassword';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
-//import Assessment from './pages/Assessment';
-import Problem from './pages/problem'; 
-import Results from './pages/Results'; // Import Results component
+import Problem from './pages/problem';
+import Results from './pages/Results';
+//import History from './pages/History';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -13,18 +15,34 @@ function App() {
     <Router>
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route
-            path="/"
+            path="/home"
             element={
               <PrivateRoute>
                 <Home />
               </PrivateRoute>
             }
           />
-          <Route path="/problem" element={<PrivateRoute><Problem /></PrivateRoute>} />
-          <Route path="/Results" element={<PrivateRoute><Results /></PrivateRoute>} /> {/* Add Results route */}
+          <Route
+            path="/problem"
+            element={
+              <PrivateRoute>
+                <Problem />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/results"
+            element={
+              <PrivateRoute>
+                <Results />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </Router>
@@ -32,3 +50,4 @@ function App() {
 }
 
 export default App;
+
